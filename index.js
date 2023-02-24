4ca0d0580713dee9a9de308ee12dcbd96691a6c0
shahrzadblackfox <75183431+shahrzadblackfox@users.noreply.github.com>
2023-02-24 15:00:13 +0330

    comment out js cause it was not working

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function reveal() {
+/*function reveal() {
   var reveals = document.querySelectorAll(".reveal");
 
   for (var i = 0; i < reveals.length; i++) {
@@ -14,4 +14,4 @@ function reveal() {
   }
 }
 
-window.addEventListener("scroll", reveal);
+window.addEventListener("scroll", reveal);*/
